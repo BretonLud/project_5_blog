ea5050ec106b36b190ca753594f76eb7d3cdc202
BretonLud <godneo@hotmail.fr>
2024-02-29 18:32:02 +0100

    Expand SecurityController functionalities
    The SecurityController functionalities have been significantly expanded, including the implementation of login, logout, and handleLoginSubmit methods. Necessary dependencies also have been added. This improves the authentication management of the application.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,74 @@
 
 namespace App\Controller;
 
-class SecurityController
-{
+use App\Database\Connection;
+use App\Http\RedirectResponse;
+use App\Http\Response;
+use App\Repository\UserRepository;
+use App\Service\AuthService;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
+class SecurityController extends Controller
+{
+    
+    
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    public function login(): Response|RedirectResponse
+    {
+        if ($this->getUser()) {
+            return new RedirectResponse('/');
+        }
+        
+        $html = $this->twig->render('security/login.html.twig');
+        
+        return new Response($html);
+    }
+    
+    /**
+     * @return RedirectResponse
+     */
+    public function handleLoginSubmit(): RedirectResponse
+    {
+        
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            return new RedirectResponse('/login');
+        }
+        
+        $email = $_POST['email'] ?? '';
+        $password = $_POST['password'] ?? '';
+        
+        $authService = new AuthService(new UserRepository(new Connection()));
+        
+        try {
+            
+            return $authService->authenticate($email, $password);
+            
+        } catch (\Exception $exception) {
+            
+            $_SESSION['error']['login'] = $exception->getMessage();
+            
+            return new RedirectResponse('/login');
+        }
+    }
+    
+    /**
+     * @return RedirectResponse
+     */
+    public function logout(): RedirectResponse
+    {
+        if ($this->getUser()) {
+            unset($_SESSION['user']);
+            
+            header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: no-cache');
+        }
+        
+        return new RedirectResponse('/');
+    }
 }
