07d9861cd6ebe69cf16694824c38fec41f8c9651
BretonLud <godneo@hotmail.fr>
2024-03-04 21:40:50 +0100

    Create AdminController class with admin role verification
    The new controller, AdminController, extends AbstractController and includes a method for admin role verification. The 'isGrantedAdmin' method checks if the user has an admin role and control is passed to this controller when an admin role is confirmed. If not, it redirects to an appropriate location.

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Abstract\AbstractController;
+use App\Http\Response;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
+    public function index(): Response
+    {
+        $this->isGrantedAdmin($this->getUser());
+        
+        $html = $this->twig->render('admin/index.html.twig');
+        
+        return new Response($html);
+    }
+}
