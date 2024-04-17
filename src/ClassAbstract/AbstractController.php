91ec3bf9c759cb703d635f39d21e1c221634adb0
BretonLud <godneo@hotmail.fr>
2024-04-17 22:11:46 +0200

    Add Exception handling and enforce code consistency
    The Exception class is imported to handle potential errors more accurately. Error annotations and logical comparisons have been cleaned up for better code readability and consistency. Additionally, a new private function `ensureRootDefined` has been introduced to check and define the 'ROOT' if it doesn't exist.

--- src/ClassAbstract/AbstractController.php
+++ src/ClassAbstract/AbstractController.php
@@ -6,6 +6,7 @@ use App\Database\Connection;
 use App\Entity\User;
 use App\Repository\CommentRepository;
 use App\Repository\UserRepository;
+use Exception;
 use Twig\Environment;
 use Twig\Error\RuntimeError;
 use Twig\Extension\CoreExtension;
@@ -19,9 +20,11 @@ abstract class AbstractController extends AbstractSecurity
     
     /**
      * @throws RuntimeError
+     * @throws Exception
      */
     public function __construct()
     {
+        $this->ensureRootDefined();
         $this->loader = new FilesystemLoader(ROOT . '/templates');
         $this->twig = new Environment($this->loader, array(
             'debug' => true
@@ -65,8 +68,7 @@ abstract class AbstractController extends AbstractSecurity
     
     /**
      * @return void
-     * @throws RuntimeError
-     * @throws \Exception
+     * @throws Exception
      */
     private function setupTwig(): void
     {
@@ -80,7 +82,7 @@ abstract class AbstractController extends AbstractSecurity
     
     /**
      * @return void
-     * @throws \Exception
+     * @throws Exception
      */
     private function setupTwigGlobals(): void
     {
@@ -99,7 +101,7 @@ abstract class AbstractController extends AbstractSecurity
         
         $app = ['user' => $user ?: null];
         
-        if ($user and $user->getRole() === "ADMIN") {
+        if ($user && $user->getRole() === "ADMIN") {
             $app['unapprovedCommentCount'] = $this->getUnapprovedCommentCount();
         }
         
@@ -107,7 +109,7 @@ abstract class AbstractController extends AbstractSecurity
     }
     
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function getUnapprovedCommentCount(): int
     {
@@ -115,5 +117,12 @@ abstract class AbstractController extends AbstractSecurity
         $unapprovedComments = $commentRepository->findBy(['validated' => false]);
         
         return count($unapprovedComments);
+    }
+    
+    private function ensureRootDefined(): void
+    {
+        if (!defined('ROOT')) {
+            define('ROOT', dirname(__DIR__));
+        }
     }
 }
